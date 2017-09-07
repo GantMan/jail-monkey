114362b8cbb413847c1578308f68facac8e724e0
Gustavo E. Jimenez Folta <gustavojimenez.folta@gmail.com>
2017-09-07 15:24:53 +0200

    Rename root check methods' number suffixes

--- android/src/main/java/com/gantix/JailMonkey/JailMonkeyModule.java
+++ android/src/main/java/com/gantix/JailMonkey/JailMonkeyModule.java
@@ -76,13 +76,13 @@ public class JailMonkeyModule extends ReactContextBaseJavaModule {
    */
   private boolean isJailBroken() {
     if (android.os.Build.VERSION.SDK_INT >= 23) {
-      return checkRootMethod2() || checkRootMethod3();
+      return checkRootMethod1() || checkRootMethod2();
     } else {
       return isSuperuserPresent() || canExecuteCommand("/system/xbin/which su");
     }
   }
 
-	private static boolean checkRootMethod2() {
+	private static boolean checkRootMethod1() {
 	    String[] paths = { "/system/app/Superuser.apk", "/sbin/su", "/system/bin/su", "/system/xbin/su", "/data/local/xbin/su", "/data/local/bin/su", "/system/sd/xbin/su",
 	            "/system/bin/failsafe/su", "/data/local/su" };
 	    for (String path : paths) {
@@ -91,7 +91,7 @@ public class JailMonkeyModule extends ReactContextBaseJavaModule {
 	    return false;
 	}
 
-	private static boolean checkRootMethod3() {
+	private static boolean checkRootMethod2() {
 	    Process process = null;
 	    try {
 	        process = Runtime.getRuntime().exec(new String[] { "/system/xbin/which", "su" });
