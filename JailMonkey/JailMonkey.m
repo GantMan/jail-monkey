d69f4d9fa275fb6bcc2337344bcd73b47f455a1e
Denis Junuzonic <denis.junuzonic@fleetster.net>
2019-03-14 14:43:38 +0100

    removed the old reference and added the missing library

--- JailMonkey/JailMonkey.m
+++ JailMonkey/JailMonkey.m
@@ -8,6 +8,7 @@
 
 #import "JailMonkey.h"
 @import UIKit;
+@import Darwin.sys.sysctl;
 
 static NSString * const JMJailbreakTextFile = @"/private/jailbreak.txt";
 static NSString * const JMisJailBronkenKey = @"isJailBroken";
@@ -83,26 +84,6 @@ RCT_EXPORT_MODULE();
     [[NSFileManager defaultManager] removeItemAtPath:JMJailbreakTextFile error:nil];
     
     return grantsToWrite;
-}
-
-- (BOOL)isDebugged{
-  int                 junk;
-  int                 mib[4];
-  struct kinfo_proc   info;
-  size_t              size;
-
-  info.kp_proc.p_flag = 0;
-
-  mib[0] = CTL_KERN;
-  mib[1] = KERN_PROC;
-  mib[2] = KERN_PROC_PID;
-  mib[3] = getpid();
-
-  size = sizeof(info);
-  junk = sysctl(mib, sizeof(mib) / sizeof(*mib), &info, &size, NULL, 0);
-  assert(junk == 0);
-
-  return ( (info.kp_proc.p_flag & P_TRACED) != 0 );
 }
 
 - (BOOL)isDebugged{
