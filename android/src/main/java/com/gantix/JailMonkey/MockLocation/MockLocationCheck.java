437a5dab3626c32651ec22f91d7a6ada9170b044
Zankich <nicholas.zankich@smarkets.com>
2020-04-21 19:23:20 -0700

    Fixed support for android mock location detection on versions Marshmellow and above

--- android/src/main/java/com/gantix/JailMonkey/MockLocation/MockLocationCheck.java
+++ android/src/main/java/com/gantix/JailMonkey/MockLocation/MockLocationCheck.java
@@ -2,15 +2,52 @@ package com.gantix.JailMonkey.MockLocation;
 
 import android.content.Context;
 import android.provider.Settings;
+import android.os.Build;
+import android.util.Log;
+import com.facebook.react.bridge.ReactContext;
+import android.content.pm.PackageManager;
+
+import java.util.List;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.PackageInfo;
+import android.location.LocationManager;
+
+import java.lang.IllegalArgumentException;
 
 public class MockLocationCheck {
-
-    //returns true if mock location enabled, false if not enabled.
     public static boolean isMockLocationOn(Context context) {
-        if ("0".equals(Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ALLOW_MOCK_LOCATION))) {
-            return false;
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return "0".equals(Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ALLOW_MOCK_LOCATION));
         } else {
-            return true;
+            PackageManager pm = context.getPackageManager();
+            List<ApplicationInfo> packages =
+                    pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+            for (ApplicationInfo applicationInfo : packages) {
+                try {
+                    PackageInfo packageInfo = pm.getPackageInfo(applicationInfo.packageName,
+                            PackageManager.GET_PERMISSIONS);
+
+                    // Get Permissions
+                    String[] requestedPermissions = packageInfo.requestedPermissions;
+
+                    if (requestedPermissions != null) {
+                        for (int i = 0; i < requestedPermissions.length; i++) {
+                            if (requestedPermissions[i]
+                                    .equals("android.permission.ACCESS_MOCK_LOCATION")
+                                    && !applicationInfo.packageName.equals(context.getPackageName())) {
+                                return true;
+                            }
+                        }
+                    }
+                } catch (NameNotFoundException e) {
+                    Log.e("Mock location check error ", e.getMessage());
+                }
+            }
+
+            return false;
         }
     }
 }
